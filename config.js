98d08712ad1e5e5875ba94de01fcac0ef31b25cc
AL00448 <hagirin@yamaha-motor.co.jp>
2020-08-26 09:14:20 +0900

    edit redirect uri

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const msalConfig = {
   auth: {
 	  //アプリの登録でのアプリケーションIDとリダイレクトURIの設定（office365ymsl_githubwebsite）
     clientId: '2aa23749-177c-4650-be79-a7f92385eac1',
-    redirectUri: 'https://office365ymslchatbot.azurewebsites.net'
+    redirectUri: 'https://office365ymslchatbotmodify.azurewebsites.net'
   },
   cache: {
     cacheLocation: "sessionStorage",
